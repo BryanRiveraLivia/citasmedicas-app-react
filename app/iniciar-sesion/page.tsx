bf95cab50ca8b42b3cb79396afade6443c547d6d
Bryan Rivera <bryan.riv09@live.com>
2023-06-22 00:09:01 -0500

    instalación de proyecto + bootstrap

---
+++ app/iniciar-sesion/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+          modi reiciendis optio odio magnam ea porro sit, sunt eius, dolores
+          possimus corrupti vel cum soluta culpa totam? Corporis, quaerat sed.
+        </div>
+      </div>
+    </div>
+  );
+}
